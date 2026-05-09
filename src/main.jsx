2e09e37e54708f004a9975fe54ea92f81c7a839c
chridtopherpdc <chridtopherpdc@gmail.com>
2026-05-09 13:01:56 +0200

    Add main.jsx to render the App component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../mundial2026.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
